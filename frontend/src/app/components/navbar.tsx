7338a6075a28b4fb6d67bc94c76a4bee115deb35
Kalyan Rohan <s3958905@student.rmit.edu.au>
2023-08-31 17:20:34 +1000

    added link to logo image

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -52,11 +52,13 @@ export const Navbar = ({ activePath }: NavbarProps) => {
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
+                                    <Link href="/">
                                     <img
                                         className="h-8 w-auto"
                                         src="/logo.png"
                                         alt="Logo"
                                     />
+                                    </Link>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
