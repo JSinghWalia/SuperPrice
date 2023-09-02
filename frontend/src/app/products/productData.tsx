65bf00af01de11eafa2f65619f491c393e85f5c8
JSinghWalia <s3899925@student.rmit.edu.au>
2023-09-02 18:09:07 +1000

    image urls changed

--- frontend/src/app/products/productData.tsx
+++ frontend/src/app/products/productData.tsx
@@ -1,27 +1,26 @@
-// Dummy product data (replace this with your actual data)
 export const productsData = [
     {
         id: 't1',
         name: 'T-Shirt',
-        imageSrc: '/tshirtSample.png',
+        imageSrc: '/tshirt.png',
         price: '$19.99'
     },
     {
         id: 't2',
         name: 'Coke',
-        imageSrc: '/cokeSample.png',
+        imageSrc: '/coke.png',
         price: '$3.99'
     },
     {
         id: 't3',
         name: 'Molten Basketball',
-        imageSrc: '/basketballSample.png',
+        imageSrc: '/basketball.png',
         price: '$69'
     },
     {
         id: 't4',
         name: 'Samsung Watch',
-        imageSrc: '/watchSample.webp',
+        imageSrc: '/watch.webp',
         price: '$200'
     }
 ];
