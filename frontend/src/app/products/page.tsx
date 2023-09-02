12654bc8f9fa80777ef5e2d63949a37940772b72
JSinghWalia <s3899925@student.rmit.edu.au>
2023-09-02 18:52:01 +1000

    added add to cart button

--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { productsData } from './productData'; // Make sure this path is correct
 import { Navbar } from '../components/navbar';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import * as React from "react";
 
 
 export default function Products() {
@@ -18,6 +20,11 @@ export default function Products() {
                             <Image src={product.imageSrc} alt={product.name} width={200} height={200} />
                             <h2>{product.name}</h2>
                             <p>{product.price}</p>
+                            <button className="add-to-cart-button">
+                                <span>Add to Cart</span>
+                                <i className="material-icons"></i> {/* Example icon */}
+                                <ShoppingCartIcon className="h-6 w-6" />
+                            </button>
                         </div>
                     </Link>
                 ))}
