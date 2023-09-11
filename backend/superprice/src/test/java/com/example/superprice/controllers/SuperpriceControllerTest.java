2b846a5433647554f37034de2cb73c513eb70b4f
“[Ramon <s3840883@student.rmit.edu.au>
2023-09-11 22:22:14 +1000

    updated controller directory

---
+++ backend/superprice/src/test/java/com/example/superprice/controllers/SuperpriceControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.superprice.controllers;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SuperpriceControllerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+}
