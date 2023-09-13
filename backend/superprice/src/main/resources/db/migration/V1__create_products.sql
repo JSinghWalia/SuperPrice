7ee00d13c58f0290d0aff1ba043e614ac1079061
Manik Thapliyal <s3953664@student.rmit.edu.au>
2023-09-13 17:18:19 +1000

    fixed data insertion

--- backend/superprice/src/main/resources/db/migration/V1__create_products.sql
+++ backend/superprice/src/main/resources/db/migration/V1__create_products.sql
@@ -10,14 +10,14 @@ CREATE TABLE products
     quantity    INT             NOT NULL
 );
 
-INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+INSERT INTO products (name, description, store, imageURL, price, quantity)
 VALUES ('T-Shirt', 'This is a pretty cool shirt.', 'Coles', '/tshirt.png', 19.99, 20);
 
-INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+INSERT INTO products (name, description, store, imageURL, price, quantity)
 VALUES ('Coke', 'A totally healthy beverage that is very tasty.', 'Woolworths', '/cokeBottle.png', 3.99, 18);
 
-INSERT INTO products (id, name, description, store, imageURL, price, quantity)
-VALUES ('Molten Basketball', 'A nice basketball that is not overpriced at all.', 'Woolworhts', '/basketball.png', 69, 6)
+INSERT INTO products (name, description, store, imageURL, price, quantity)
+VALUES ('Molten Basketball', 'A nice basketball that is not overpriced at all.', 'Woolworhts', '/basketball.png', 69, 6);
 
-INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+INSERT INTO products (name, description, store, imageURL, price, quantity)
 VALUES ('Samsung Watch', 'A new smart watch which is totally necessary.', 'Coles', '/watch.webp', 200, 3);
