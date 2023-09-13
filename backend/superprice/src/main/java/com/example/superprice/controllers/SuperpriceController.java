0501a6bec9f74f05248516a120051db8d74ae91b
Manik Thapliyal <s3953664@student.rmit.edu.au>
2023-09-13 21:39:24 +1000

    started implementation of search

--- backend/superprice/src/main/java/com/example/superprice/controllers/SuperpriceController.java
+++ backend/superprice/src/main/java/com/example/superprice/controllers/SuperpriceController.java
@@ -2,6 +2,8 @@ package com.example.superprice.controllers;
 
 import com.example.superprice.model.Product;
 import com.example.superprice.services.SuperpriceService;
+import com.example.superprice.services.SuperpriceServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value="v1/superprice")
 public class SuperpriceController {
-    private SuperpriceService service;
-
+    private SuperpriceService service = new SuperpriceServiceImpl();
     @GetMapping
     public String all() {
         return "Hello World";
--- backend/superprice/src/main/java/com/example/superprice/services/SuperpriceService.java
+++ backend/superprice/src/main/java/com/example/superprice/services/SuperpriceService.java
@@ -1,9 +1,11 @@
 package com.example.superprice.services;
 
 import com.example.superprice.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 public interface SuperpriceService {
+
     public ArrayList<Product> searchKeyword(String keyword);
 }
