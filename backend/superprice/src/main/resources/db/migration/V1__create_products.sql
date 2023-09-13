869981122217f522ab0b24b6cb563345b766afb0
Manik Thapliyal <s3953664@student.rmit.edu.au>
2023-09-13 16:57:07 +1000

    insert table values

--- backend/superprice/src/main/resources/db/migration/V1__create_products.sql
+++ backend/superprice/src/main/resources/db/migration/V1__create_products.sql
@@ -6,7 +6,18 @@ CREATE TABLE products
     description VARCHAR(255)    NOT NULL,
     store       VARCHAR(255)    NOT NULL,
     imageURL    VARCHAR(255)    NOT NULL,
-    price       INT             NOT NULL,
+    price       FLOAT           NOT NULL,
     quantity    INT             NOT NULL
 );
 
+INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+VALUES ('T-Shirt', 'This is a pretty cool shirt.', 'Coles', '/tshirt.png', 19.99, 20);
+
+INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+VALUES ('Coke', 'A totally healthy beverage that is very tasty.', 'Woolworths', '/cokeBottle.png', 3.99, 18);
+
+INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+VALUES ('Molten Basketball', 'A nice basketball that is not overpriced at all.', 'Woolworhts', '/basketball.png', 69, 6)
+
+INSERT INTO products (id, name, description, store, imageURL, price, quantity)
+VALUES ('Samsung Watch', 'A new smart watch which is totally necessary.', 'Coles', '/watch.webp', 200, 3);
