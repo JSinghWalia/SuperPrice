3fa2c6eeecc5648626cc80135920b179837adaa9
Clarence <S3943755@student.rmit.edu.au>
2023-09-15 13:20:27 +1000

    Update to database to make it flexible for future entities

--- backend/superprice/src/main/resources/db/migration/V1__create_products.sql
+++ backend/superprice/src/main/resources/db/migration/V1__create_products.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS products;
 CREATE TABLE products
 (
-    id          INT PRIMARY KEY AUTO_INCREMENT,
-    name        VARCHAR(255)    NOT NULL,
-    description VARCHAR(255)    NOT NULL,
-    store       VARCHAR(255)    NOT NULL,
-    imageURL    VARCHAR(255)    NOT NULL,
-    price       FLOAT           NOT NULL,
-    quantity    INT             NOT NULL
+    productId          INT PRIMARY KEY AUTO_INCREMENT,
+    name               VARCHAR(255)    NOT NULL,
+    description        VARCHAR(255)    NOT NULL,
+    store              VARCHAR(255)    NOT NULL,
+    imageURL           VARCHAR(255)    NOT NULL,
+    price              FLOAT           NOT NULL,
+    productQuantity    INT             NOT NULL
 );
