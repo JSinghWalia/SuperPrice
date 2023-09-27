ad5479062056faf9d359c534e91f3e57a24d4cda
“[Ramon <s3840883@student.rmit.edu.au>
2023-09-27 13:09:19 +1000

    Updated database records

--- backend/superprice/src/main/resources/db/migration/V2__insert_products.sql
+++ backend/superprice/src/main/resources/db/migration/V2__insert_products.sql
@@ -2,8 +2,8 @@ INSERT INTO products (name, description, store, imageURL, price, productQuantity
 VALUES
     ('T-Shirt', 'This is a pretty cool shirt.', 'Coles', '/tshirt.png', 19.99, 20, false, false),
     ('Coke', 'A totally healthy beverage that is very tasty.', 'Woolworths', '/cokeBottle.png', 3.99, 18, false, true),
-    ('Molten Basketball', 'A nice basketball that is not overpriced at all.', 'Woolworhts', '/basketball.png', 69, 6, true, false),
-    ('Samsung Watch', 'A new smart watch which is totally necessary.', 'Coles', '/watch.webp', 200, 3, true, true),
+    ('Molten Basketball', 'A nice basketball that is not overpriced at all.', 'Woolworhts', '/basketball.png', 69, 6, 0.05, false),
+    ('Samsung Watch', 'A new smart watch which is totally necessary.', 'Coles', '/watch.webp', 200, 3, 0.10, true),
     ('Coke', 'A totally healthy bevemvnrage that is very tasty.', 'Coles', '/cokeBottle.png', 3.99, 18, false, false)
 ;
 
