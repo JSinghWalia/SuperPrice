f88c5d0136a26308d836837a2c5c03c1e7496722
Clarence <S3943755@student.rmit.edu.au>
2023-10-05 23:21:10 +1100

    Updated Tests to have their own db

---
+++ backend/superprice/src/test/resources/db/migration/V1__create_products.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products
+(
+    productId          INT PRIMARY KEY AUTO_INCREMENT,
+    name               VARCHAR(255)    NOT NULL,
+    description        VARCHAR(255)    NOT NULL,
+    store              VARCHAR(255)    NOT NULL,
+    imageURL           VARCHAR(255)    NOT NULL,
+    price              FLOAT           NOT NULL,
+    productQuantity    INT             NOT NULL,
+    promotion          FLOAT           NOT NULL,
+    notification       BOOL            NOT NULL
+);
+
+CREATE TABLE cart (
+    cartId INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE cartitem (
+                          cartItemId        INT PRIMARY KEY AUTO_INCREMENT,
+                          cartItemQuantity  INT NOT NULL,
+                          cartId            INT,
+                          productId         INT,
+                          foreign key (cartId) references cart(cartId),
+                          foreign key (productId) references products(productId)
+);
---
+++ backend/superprice/src/test/resources/db/migration/V2__insert_products.sql
@@ -0,0 +1,35 @@
+INSERT INTO products (name, description, store, imageURL, price, productQuantity, promotion, notification)
+VALUES
+    ('T-Shirt', 'This is a pretty cool shirt.', 'Coles', '/tshirt.png', 19.99, 20, 0, false),
+    ('Coke', 'A totally healthy beverage that is very tasty.', 'Woolworths', '/cokeBottle.png', 3.99, 18, 0, true),
+    ('Molten Basketball', 'A nice basketball that is not overpriced at all.', 'Woolworths', '/basketball.png', 69, 6, 0.05, true),
+    ('Samsung Watch', 'A new smart watch which is totally necessary.', 'Coles', '/watch.webp', 200, 3, 0.10, true),
+    ('Coke', 'A totally healthy bevemvnrage that is very tasty.', 'Coles', '/cokeBottle.png', 3.99, 18, 0, false)
+;
+
+INSERT INTO cart (cartId)
+VALUES
+    (1),
+    (2),
+    (3),
+    (4)
+;
+
+INSERT INTO cartitem (cartItemQuantity, cartId, productId)
+VALUES
+    -- CART 1
+    (2, 1, 1), -- 2 t-shirts
+    (3, 1, 2), -- 3 cokes (woolies)
+    (1, 1, 3), -- 1 Molten Basketball
+    (4, 1, 4), -- 4 Samsung Watch
+    (2, 1, 5), -- 2 cokes from coles o_O
+
+    -- CART 2
+    (1, 2, 1), -- 1 t-shirt
+    (500, 2, 3), -- 500 Molten Basketball
+
+    -- CART 3
+    (5, 3, 1), -- 5 t-shirts
+    (10, 3, 5), -- 10 cokes (coles)
+    (7, 3, 2) -- 7 cokes (woolies)
+;
