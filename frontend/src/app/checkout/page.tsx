c12a4b0cec60f8ea4a0db45480b96b5c38938a3f
Kalyan Rohan <s3958905@student.rmit.edu.au>
2023-10-06 14:02:13 +1100

    fixed navbar issue in checkout page and added more styling

--- frontend/src/app/checkout/page.tsx
+++ frontend/src/app/checkout/page.tsx
@@ -206,21 +206,9 @@ export default function CheckOut(){
     return(
         <>
         <Navbar activePath="checkout" />
-            <section className="grid grid-cols-3 flex justify-stretch" style={{ backgroundColor: "#eee" }}>
-                <div className="col-span-2 bg-white p-6 flex flex-col justify-center items-center">
+            <section className="grid grid-cols-2 flex justify-stretch" style={{ backgroundColor: "#eee" }}>
+                <div className="col-span-1 bg-white p-6 flex flex-col justify-center items-center mt-2 rounded-lg ml-2">
                     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 flex flex-col justify-center items-center">
-                        <div
-                            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-                            aria-hidden="true"
-                        >
-                            <div
-                                className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-                                style={{
-                                    clipPath:
-                                        'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                                }}
-                            />
-                            </div>
                         <div className="mx-auto max-w-2xl text-center">
                             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Checkout</h2>
                         </div>
@@ -513,11 +501,10 @@ export default function CheckOut(){
 
                     <hr className="my-4" />
                 </div>
-                <div className="p-5 col-span-1 bg-grey p-6 flex flex-col justify-center items-center">
-                    <div className="flex justify-center items-center h-full">
-                        <div className="lg:w-8/12 bg-white p-8 rounded-lg shadow-lg">
-                            <h1 className="text-2xl font-bold mb-5">Shopping Cart</h1>
-
+                <div className="col-span-1 bg-grey mt-2">
+                    <div className="w-full mx-auto">
+                        <div className="bg-white p-8 mx-auto rounded-lg shadow-lg w-fit">
+                            <h1 className="text-2xl font-bold mb-5">Order</h1>
                             <div className="grid grid-cols-3 gap-4 border-b-2 border-gray-300 pb-4 mb-4">
                                 <div className="col-span-1">Product Name:</div>
                                 <div className="col-span-1">Quantity:</div>
